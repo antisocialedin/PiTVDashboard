83b202914d4bc5080f5886afdf0d0c311d032d53
Eder Filho <ederlopes_7@hotmail.com>
2026-05-26 01:48:29 -0300

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,15 @@ import tailwindcss from "@tailwindcss/vite";
 import vue from "@vitejs/plugin-vue";
 
 // https://vite.dev/config/
-export default defineConfig({
+/* export default defineConfig({
   plugins: [vue(), tailwindcss()],
-});
+}); */
+
+export default defineConfig({
+  plugins: [vue()],
+  preview: {
+    host: '0.0.0.0',
+    port: process.env.PORT,
+    allowedHosts: ['pitvdashboard-nbp1.onrender.com']
+  }
+})
