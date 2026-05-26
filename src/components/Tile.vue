0e8413580264097c2631cecce1340df36d45df48
Eder Filho <ederlopes_7@hotmail.com>
2026-05-26 03:07:25 -0300

    Update Tile.vue

--- src/components/Tile.vue
+++ src/components/Tile.vue
@@ -17,15 +17,18 @@ defineExpose({
 <template>
   <a ref="rootEl" :href="href" :class="selected ? 'z-20' : 'z-10'">
     <div
-      class="flex justify-center items-center aspect-video w-full max-w-[320px] rounded-2xl backdrop-blur-2xl border border-white/20 hover:scale-[1.04] transition-all shadow-sm p-5"
       :class="[
         color,
         selected
           ? 'scale-[1.04] ring-4 ring-white shadow-xl z-20'
           : 'scale-100'
       ]"
+      class="w-[300px] h-[170px] rounded-2xl backdrop-blur-2xl border border-white/20 hover:scale-[1.04] transition-all shadow-sm p-5 flex justify-center items-center overflow-hidden"
     >
-      <img :src="logo" class="max-h-full max-w-full object-contain" />
+      <img
+        :src="logo"
+        class="max-w-[80%] max-h-[80%] object-contain"
+      />
     </div>
   </a>
 </template>
