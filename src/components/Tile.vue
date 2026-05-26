385e25e7551834395a04d29b4d73c52cb2807033
Eder Filho <ederlopes_7@hotmail.com>
2026-05-26 03:17:17 -0300

    Update Tile.vue

--- src/components/Tile.vue
+++ src/components/Tile.vue
@@ -1,26 +1,39 @@
 <script setup>
-import { ref } from "vue"
-
-const { logo, color, href, selected } = defineProps([
-  "logo",
-  "color",
-  "href",
-  "selected"
-])
-
-const rootEl = ref(null)
-defineExpose({
-  rootEl
-})
+const { logo, href } = defineProps(["logo", "href"])
 </script>
 
 <template>
-  <div
+  <a
+    :href="href"
     style="
-      width: 250px;
-      height: 140px;
-      background: red;
-      border-radius: 20px;
+      text-decoration: none;
+      display: flex;
+      justify-content: center;
     "
-  ></div>
+  >
+    <div
+      style="
+        width: 250px;
+        height: 140px;
+        border-radius: 20px;
+        background: rgba(255,255,255,0.08);
+        border: 1px solid rgba(255,255,255,0.2);
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        overflow: hidden;
+        padding: 20px;
+        backdrop-filter: blur(20px);
+      "
+    >
+      <img
+        :src="logo"
+        style="
+          width: 100%;
+          height: 100%;
+          object-fit: contain;
+        "
+      />
+    </div>
+  </a>
 </template>
